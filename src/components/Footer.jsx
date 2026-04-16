91f37850ead4a89ab58416a04784eec3d8f2bcd6
Sakialamin01 <sakialaminwork@gmail.com>
2026-04-16 07:29:35 +0600

    Footer Section Add

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="bg-green-900 text-white text-center py-10 mt-10">
+      <h2 className="text-2xl font-bold">KeenKeeper</h2>
+      <p className="text-sm mt-2">
+        Your personal shelf of meaningful connections.
+      </p>
+      <p className="mt-4 text-xs">© 2026 KeenKeeper</p>
+    </div>
+  );
+};
+
+export default Footer;
